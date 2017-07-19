2c241b26f0aed808618cf0024fc884f37c1e3eb7
bec-f <becftemp2234@gmail.com>
2017-07-19 14:12:47 +0000

    Attempt at implementing a rate-limit sink
    Added two files modeled after other sinks in attempt to create a rate-limit sink.
    Updated all of the rate code to use an integer rate rather than a double rate.
    Not sure how to calculate rate or how I would make the call to write to the log.
    Attempt to make changes discussed in #108.

---
+++ include/tlog/rate_limit_sink.h
@@ -0,0 +1,31 @@
+#ifndef _TLOG_RATE_LIMIT_SINK_H
+#define _TLOG_RATE_LIMIT_SINK_H
+
+#include <tlog/sink.h>
+
+/** Rate Limit sink type */
+extern const struct tlog_sink_type tlog_rate_limit_sink_type;
+
+
+/**
+ * Create (allocate and initialize) a log sink.
+ *
+ * @param psink     Location for created sink pointer, set to NULL
+ *                  in case of error.
+ * @param dsink     Location of the destination/log sink to write to.
+ * @param rate      Maximum cutoff rate for writing to the log.
+ *
+ * @return Global return code.
+ */
+static inline tlog_grc
+tlog_rate_limit_sink_create(struct tlog_sink **psink,
+                            strut tlog_sink **dsink,
+                            uint64_t           rate)
+{
+    assert(psink != NULL);
+    assert(dsink != NULL);
+    return tlog_sink_create(psink, &tlog_rate_limit_sink_type,
+                            dsink, rate);
+}
+
+#endif /* _TLOG_RATE_LIMIT_SINK_H */
---
+++ lib/rate_limit_sink.c
@@ -0,0 +1,93 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <tlog/rate_limit_sink.h>
+
+/** Rate Limit sink instance */
+struct rate_limit_sink {
+    struct tlog_sink        sink;       /**< Abstract sink instance */
+    struct tlog_sink       *dsink;      /**< "Destination" (logging) sink */
+    int                     rate;       /**< Cutoff rate */
+}
+
+static bool
+tog_rate_limit_sink_is_valid(const struct tlog_sink *sink)
+{
+    struct tlog_rate_limit_sink *rate_limit_sink =
+                                (struct tlog_rate_limit_sink *)sink;
+    return rate_limit_sink != NULL;
+}
+
+static void
+tlog_rate_limit_sink_cleanup(struct tlog_sink *sink)
+{
+    struct tlog_rate_limit_sink *rate_limit_sink =
+                                (struct tlog_rate_limit_sink *)sink;
+    rate_limit_sink->dsink = NULL;
+    (void) rate_limit_sink; /* unused unless asserts are enabled */
+
+    assert(rate_limit_sink != NULL);
+}
+
+static tlog_grc
+tlog_rate_limit_sink_init(struct tlog_sink *sink, va_list ap)
+{
+    struct tlog_rate_limit_sink *rate_limit_sink =
+                                 (struct tlog_rate_limit_sink *)sink;
+    struct tlog_sink *dpsink = va_arg(ap, struct tlog_sink *);
+
+    if(dsink == NULL){
+      goto error;
+    }
+    rate_limit_sink->dsink = dpsink;
+    rate_limit_sink->rate = va_arg(ap, int)
+    return TLOG_RC_OK;
+
+error:
+    tlog_rate_limit_sink_cleanup(sink);
+    return TLOG_GRC_ERRNO;
+}
+
+static tlog_grc
+tlog_sink_write(struct tlog_sink *sink,
+                const struct tlog_pkt *pkt,
+                struct tlog_pkt_pos *ppos,
+                const struct tlog_pkt_pos *end)
+{
+    struct tlog_rate_limit_sink *rate_limit_sink =
+                                 (struct tlog_rate_limit_sink *)sink;
+    tlog_grc grc;
+
+    if(pkt->type != TLOG_PKT_TYPE_IO || rate_limit_sink->rate == 0){
+        grc = tlog_sink_write(rate_limit_sink->dsink, &pkt, &log_pos, NULL);
+        if (grc != TLOG_RC_OK &&
+            grc != TLOG_GRC_FROM(errno, EINTR)) {
+
+            tlog_errs_pushc(perrs, grc);
+            tlog_errs_pushs(perrs, "Failed logging terminal data");
+            return_grc = grc;
+            goto error;
+        }
+    }
+    else {
+        int cur_rate = //Calulate rate. Not sure how to do yet
+        if(cur_rate < rate_limit_sink->rate) {
+            tlog_sink_write(rate_limit_sink->dsink, &pkt, &log_pos, NULL);
+        }
+        else {
+            tlog_pkt_pos_move_past(rate_limit_sink->dsink, &pkt);
+        }
+    }
+}
+
+
+
+const struct tlog_sink_type tlog_rate_limit_sink_type = {
+    .size       = sizeof(struct tlog_rate_limit_sink),
+    .init       = tlog_rate_limit_sink_init,
+    .cleanup    = tlog_rate_limit_sink_cleanup,
+    .is_valid   = tlog_rate_limit_sink_is_valid,
+    .write      = tlog_rate_limit_sink_write,
+};
+
